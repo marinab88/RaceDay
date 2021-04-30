ac838aa75845368f2d735839b9e30086098a0d45
Marina <bucsamarina03@gmail.com>
2021-04-30 15:17:15 +0300

    Registered runners for the race and give them instructions on race day

---
+++ add.js
@@ -0,0 +1,15 @@
+let raceNumber=Math.floor(Math.random()*1000);
+let earlyRegistered=true;
+let age='18';
+
+if (age>='18' && earlyRegistered) { //Checks whever the runner is an adult and regostered early.
+    raceNumber+=1000;               // if it is true, their race numbee+1000
+} 
+
+if (age>='18' && earlyRegistered) {
+  console.log(`You will race at 9:30 am and your race number is ${raceNumber}`);
+} else if (age>='18' && !earlyRegistered) {
+  console.log(`You will race at 11:00 am and your race number is ${raceNumber}`);
+} else if (age<'18') {
+  console.log(`You will race at 12:30 am and your race number is ${raceNumber}`);
+}
